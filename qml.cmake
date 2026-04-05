e6ff24c211fa977425bd336ae72b9c751cc75d3d
kraxarn <kraxie@protonmail.com>
2026-04-05 18:14:20 +0200

    Initial project files

---
+++ qml.cmake
@@ -0,0 +1,13 @@
+# QML files are in qml/
+if (QT_KNOWN_POLICY_QTP0004)
+	qt_policy(SET QTP0004 NEW)
+endif ()
+
+qt_add_qml_module(${APP_NAME}
+	URI "kraxarn.${APP_NAME}"
+	VERSION "${PROJECT_VERSION}"
+	NO_RESOURCE_TARGET_PATH
+
+	QML_FILES
+	qml/Main.qml
+)
